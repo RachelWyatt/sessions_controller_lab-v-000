247f00a3c55e7d3f7596d1e2864db891a6bad03c
Rachel Wyatt <rw126@evansville.edu>
2019-03-04 13:40:13 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:username] = params[:username]
+    session[:name] = params[:name]
     redirect_to '/login'
   end
 
   def destroy
-    session.delete :username
+    session.delete :name
   end
 end
