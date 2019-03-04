9043e2d0bd5662ae0b7e001a6d7c44874074a80a
Rachel Wyatt <rw126@evansville.edu>
2019-03-04 13:45:56 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     session[:name] = params[:name]
-    if session[:name] == nil
+    if session[:name].nil?
       redirect_to '/login'
     else
       redirect_to '/'
